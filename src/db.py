70ea035ae19641ab2b99cb5f6a5238a0b512d3da
turkey126 <isamu.takimoto.136@gmail.com>
2020-05-11 01:44:11 +0900

    mod docker-comporse that mariadb charset change

--- src/db.py
+++ src/db.py
@@ -5,5 +5,6 @@ conn = pymysql.connect(
     user=os.environ["MYSQL_USER"],
     passwd=os.environ["MYSQL_ROOT_PASSWORD"],
     host=os.environ["MYSQL_HOST"],
-    db=os.environ["MYSQL_DATABASE"]
+    db=os.environ["MYSQL_DATABASE"],
+    charset="utf8"
 )
